b1f99bd6d9d13ea40644b957abc7a07d48793882
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-02 18:30:12 +0200

    Codeforces 1030_A

---
+++ Codeforcesproblemsets/1030A_InSearchOfAnEasyProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1030A_InSearchOfAnEasyProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "EASY";
+            int n = int.Parse(Console.ReadLine());
+            string[] tokens = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                if (int.Parse(tokens[i]) == 1)
+                    result = "HARD";
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
