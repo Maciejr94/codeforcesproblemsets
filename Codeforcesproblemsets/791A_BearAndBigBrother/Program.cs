cf1046626e2d7d3d6f936c971888d11d40507c1a
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-02 19:00:47 +0200

    Codeforces 791A

---
+++ Codeforcesproblemsets/791A_BearAndBigBrother/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _791A_BearAndBigBrother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int i = 0;
+            string[] tokens = Console.ReadLine().Split();
+            a = int.Parse(tokens[0]);
+            b = int.Parse(tokens[1]);
+            while (a <= b)
+            {
+                i++;
+                a *= 3;
+                b *= 2;
+            }
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
