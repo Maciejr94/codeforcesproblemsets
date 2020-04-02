e1c2df86af872132c6ada5110dfac57feb25d3b4
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-02 18:04:48 +0200

    Codeforces 977A

---
+++ Codeforcesproblemsets/977A_WrongSubtraction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _977A_WrongSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;
+            string[] tokens = Console.ReadLine().Split();
+            n = int.Parse(tokens[0]);
+            k = int.Parse(tokens[1]);
+            for(int i = 0; i <k; i++)
+                if (n % 10 == 0)
+                    n /= 10;
+                else
+                    n--;
+
+            Console.WriteLine(n);
+            Console.ReadLine();
+        }
+    }
+}
