9913690bec64feeb298eeddf9b918c946ddb07ba
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-16 18:23:11 +0200

    Codeforces 1186A

---
+++ Codeforcesproblemsets/1186A_VusTheCissackAndAContest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1186A_VusTheCissackAndAContest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Array.ConvertAll(Console.ReadLine().Split(), i => int.Parse(i));
+            Console.WriteLine((input[1] >= input[0] && input[2] >= input[0]) ? "Yes" : "No");
+            Console.ReadLine();
+        }
+    }
+}
