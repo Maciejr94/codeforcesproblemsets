6b6858dbf42387df713cbd1caa8e32e0a1381f54
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-16 17:27:54 +0200

    Codeforces 959A

---
+++ Codeforcesproblemsets/959A_MahmoudAndEhabAndTheEvenOddGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _959A_MahmoudAndEhabAndTheEvenOddGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n % 2 == 0 ? "Mahmoud" : "Ehab");
+            Console.ReadLine();
+        }
+    }
+}
