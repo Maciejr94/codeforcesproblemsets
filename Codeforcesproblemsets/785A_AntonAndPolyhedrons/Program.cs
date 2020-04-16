ab978aa097b84b300d71f63ccfb043c9fbed00ee
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-16 14:53:25 +0200

    Codeforces 785A

---
+++ Codeforcesproblemsets/785A_AntonAndPolyhedrons/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _785A_AntonAndPolyhedrons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n = int.Parse(Console.ReadLine());
+            int result = 0;
+            while (i < n)
+            {
+                string word = Console.ReadLine().ToLower();
+                switch (word)
+                {
+                    case "tetrahedron":
+                        result += 4;
+                        break;
+                    case "cube":
+                        result += 6;
+                        break;
+                    case "octahedron":
+                        result += 8;
+                        break;
+                    case "dodecahedron":
+                        result += 12;
+                        break;
+                    case "icosahedron":
+                        result += 20;
+                        break;
+
+                }
+                i++;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
