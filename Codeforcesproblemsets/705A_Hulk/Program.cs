83180088b2e70c2872d5a5fe46ce811db6965525
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-16 14:41:42 +0200

    Codeforces 785A

--- Codeforcesproblemsets/705A_Hulk/Program.cs
+++ Codeforcesproblemsets/705A_Hulk/Program.cs
@@ -10,6 +10,26 @@ namespace _705A_Hulk
     {
         static void Main(string[] args)
         {
+            string[] words = { "I hate", "I love" };
+            string[] caps = { " that", " it" };
+            int n = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                    result += words[0];
+                else
+                    result += words[1];
+
+                if (i == n - 1)
+                    result += caps[1];
+                else
+                    result += caps[0];
+
+                result += " ";
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
