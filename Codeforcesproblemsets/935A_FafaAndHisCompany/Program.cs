3b78cc89bd7b9fe8f4e60cda2f166428d531a70f
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-16 16:57:56 +0200

    Codeforces 935A

---
+++ Codeforcesproblemsets/935A_FafaAndHisCompany/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _935A_FafaAndHisCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 1; i < n; i++)
+                if ((n - i) % i == 0)
+                    result++;
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
