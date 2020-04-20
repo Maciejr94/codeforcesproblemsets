063ffbb89ceeac8684a663bbc41bc5e8c06658bd
Maciej Rutkowski <maciejr94@gmail.com>
2020-04-20 17:28:34 +0200

    Codeforces 1223A

---
+++ Codeforcesproblemsets/1223A_CME/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1223A_CME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = int.Parse(Console.ReadLine());
+            for (int i = 0; i < q; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n == 2) Console.WriteLine("2");
+                else if (n % 2 == 1) Console.WriteLine("1");
+                else if (n % 2 == 0 && n > 2) Console.WriteLine("0");
+            }
+        }
+    }
+}
