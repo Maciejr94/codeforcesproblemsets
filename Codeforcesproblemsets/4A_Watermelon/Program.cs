639cb145a403c4cfa7a38ab62374ab2a7c6bb6e8
Maciej Rutkowski <maciejr94@gmail.com>
2020-06-23 16:57:18 +0200

    Codeforces 4A

---
+++ Codeforcesproblemsets/4A_Watermelon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4A_Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = int.Parse(Console.ReadLine());
+            if (q > 2 && q % 2 == 0)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            
+            Console.ReadLine();
+        }
+    }
+}
