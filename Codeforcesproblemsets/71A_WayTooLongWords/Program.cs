21911e69659f4647b47836c448193a743a83d936
Maciej Rutkowski <maciejr94@gmail.com>
2020-06-23 17:20:43 +0200

    Codeforces 71A

---
+++ Codeforcesproblemsets/71A_WayTooLongWords/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _71A_WayTooLongWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < n; i++)
+                list.Add(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                if(list[i].Length > 10)
+                {
+                    list[i] = list[i].First().ToString() + (list[i].Length - 2) + list[i].Last().ToString();
+                }
+            }
+
+
+            foreach (string s in list)
+                Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
