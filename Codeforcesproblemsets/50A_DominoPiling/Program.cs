644f06fd017c992a31dbcb66d2dc5be1689b43df
Maciej Rutkowski <maciejr94@gmail.com>
2020-07-06 19:24:57 +0200

    Codeforces 50A

---
+++ Codeforcesproblemsets/50A_DominoPiling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50A_DominoPiling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nk = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+            Console.WriteLine( (nk[0]*nk[1]) / 2);
+            Console.ReadLine();
+        }
+    }
+}
