fe436e99cac16d683d663551ef796581d62dedc8
Maciej Rutkowski <maciejr94@gmail.com>
2020-07-06 13:42:40 +0200

    Codeforces 231A

---
+++ Codeforcesproblemsets/231A_Team/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _231A_Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for(int i = 0; i < n; i++)
+                list.Add(Console.ReadLine().Replace(" ",""));
+
+
+            foreach(string s in list)
+            {
+                int semiResult = 0;
+                foreach(char c in s)
+                    semiResult += c - '0';
+                
+                if (semiResult >= 2)
+                    result++;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
