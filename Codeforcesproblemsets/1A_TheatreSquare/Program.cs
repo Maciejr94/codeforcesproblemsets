c893a2f622608d270c89679ab224c9305da3ad49
Maciej Rutkowski <maciejr94@gmail.com>
2020-07-13 15:20:36 +0200

    Codeforces 1A/1B

---
+++ Codeforcesproblemsets/1A_TheatreSquare/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal [] nums = Console.ReadLine().Split().Select(i => decimal.Parse(i)).ToArray();
+            Console.WriteLine(Math.Ceiling(nums[0] / nums[2]) * Math.Ceiling(nums[1] / nums[2]));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Codeforcesproblemsets/1B_Spreadsheets/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1B_Spreadsheets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            List<string> result = new List<string>();
+            for (int i = 0; i < n; i++)
+                list.Add(Console.ReadLine());
+
+            foreach(string s in list)
+            {
+                char[] c = s.ToCharArray();
+                if(s.IndexOf('R') == 0 && s.IndexOf('C') > 1 && char.IsDigit(s[1]))
+                {
+                    int x = s.IndexOf('C');
+                    string a = string.Empty;
+                    for (int b = x + 1; b < c.Length; b++)
+                        a += c[b];
+
+                    int v = int.Parse(a);
+                    string cnvResult = string.Empty;
+                    while(v > 0)
+                    {
+                        cnvResult = (char)('A' + ( v - 1) % 26) + cnvResult;
+                        v = (v - 1) / 26;
+                    }
+            
+                    int y = s.IndexOf('R');
+                    for (int b = y + 1; b < x; b++)
+                        cnvResult += c[b];
+
+                    result.Add(cnvResult);
+                }
+                else {
+                    string t = string.Empty;
+                    string cnvResult = "R";
+                    foreach (char cz in s.ToCharArray())
+                        if (char.IsDigit(cz))
+                            cnvResult += cz;
+                        else
+                            t += cz;
+
+                    cnvResult += "C";
+                    int num = 0;
+                    for (int m = 0; m < t.Length; m++)
+                        num = num * 26 + (t[m] - 'A' + 1);
+
+                    cnvResult += num;
+                    result.Add(cnvResult);
+                }
+            }
+
+
+            foreach (string s in result)
+                Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
